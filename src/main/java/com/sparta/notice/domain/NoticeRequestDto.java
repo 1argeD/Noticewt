8978c0ed341e1325045740a42f9a3c4b1fec036d
1argeD <94172354+1argeD@users.noreply.github.com>
2022-07-27 11:39:10 +0900

    오류값 반

--- src/main/java/com/sparta/notice/domain/NoticeRequestDto.java
+++ src/main/java/com/sparta/notice/domain/NoticeRequestDto.java
@@ -5,10 +5,8 @@ import lombok.Getter;
 
 @Getter
 public class NoticeRequestDto {
-    private final String Success = String.valueOf(true);
     private String title;
     private String content;
     private String password;
     private String author;
-//    private final String error = null;
 }
